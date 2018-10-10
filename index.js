39711afbafe3a91c15ea51c9c80109118be7fbec
jjustin25 <37604889+jjustin25@users.noreply.github.com>
2018-10-10 17:46:43 -0400

    Uploading Javascript homework

---
+++ index.js
@@ -0,0 +1,48 @@
+var $tbody = document.querySelector("tbody");
+var $dateInput = document.querySelector("#datetime");
+var $stateInput = document.querySelector("#state")
+var $searchBtn = document.querySelector("#search");
+
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+
+var filteredUFO = dataSet;
+function renderTable() {
+  $tbody.innerHTML = "";
+  console.log("render is initiating")
+
+  for (var i = 0; i < filteredUFO.length; i++) {
+    var sighting = filteredUFO[i];
+    var fields = Object.keys(sighting);
+    var $row = $tbody.insertRow(i);
+    for (var j = 0; j < fields.length; j++) {
+      var field = fields[j];
+      var $cell = $row.insertCell(j);
+      $cell.innerText = sighting[field];
+    }
+  }
+}
+//Function on button click... Return date
+function handleSearchButtonClick() {
+  var filterDate = $dateInput.value.trim();
+  var filterState = $stateInput.value.trim().toLowerCase();
+  if (filterDate.length != 0){
+    filteredUFO = dataSet.filter(function(sighting) {
+      var sightingDate = sighting.datetime;
+       return sightingDate === filterDate;
+         });
+  }
+  else {
+    filteredUFO = dataSet
+  }
+  if (filterState.length != 0){
+    filteredUFO = filteredUFO.filter(function(sighting) {
+      var sightingState = sighting.state;
+       return sightingState === filterState;
+         });
+  }
+  else {
+    filteredUFO = filteredUFO
+  }
+  renderTable();
+}
+renderTable();
